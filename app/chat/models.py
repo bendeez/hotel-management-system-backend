7e8607165a09dcb6dc8175c5b6e86ccd06df1d6d
bendeez <daniellacina8@gmail.com>
2024-08-13 21:26:01 -0500

    updated tabels

--- app/chat/models.py
+++ app/chat/models.py
@@ -1,15 +1,22 @@
-from app.tools.models.base_models import BaseMixin
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Text, String, ForeignKey
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column
+)
+from sqlalchemy import ForeignKey, Text, String
+from sqlalchemy.orm import relationship
 from datetime import datetime
+from app.tools.models.base_models import BaseMixin
+from typing import Optional
+
 
 
 class Chat_Messages(BaseMixin):
     session_id: Mapped[str] = mapped_column(ForeignKey("chat_sessions.id"))
     message: Mapped[str] = mapped_column(Text)
     messenger_id: Mapped[int] = mapped_column(ForeignKey("chat_messenger.id"))
-    chat_id: Mapped[int]
     date: Mapped[datetime] = mapped_column(default=datetime.now())
+
+    messenger: Mapped["Chat_Messenger"] = relationship()
 
 
 class Chat_Messenger(BaseMixin):
@@ -37,6 +44,6 @@ class Chat_Agent(Chat_Messenger):
     }
 
 class Chat_Sessions(BaseMixin):
-    business_id: Mapped[int] = mapped_column(ForeignKey("business.id"))
-    start_time: Mapped[datetime]
-    end_time: Mapped[datetime]
+    account_id: Mapped[Optional[int]] = mapped_column(ForeignKey("accounts.id"))
+    start_time: Mapped[datetime] = mapped_column(default=datetime.now())
+    end_time: Mapped[Optional[datetime]]
