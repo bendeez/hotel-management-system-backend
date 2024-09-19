aabfdffc1acf19080f64ccdd5b51f5010b746b3f
bendeez <blaziken@MSI>
2024-09-19 08:22:47 -0500

    added imports to base file

--- admin/app/tools/domain/base.py
+++ admin/app/tools/domain/base.py
@@ -1,1 +1,8 @@
-
+from app.accounts.domain.models import *
+from app.business.domain.models import *
+from app.business_user.domain.models import *
+from app.chat.domain.models import *
+from app.facility.domain.models import *
+from app.hotels.domain.models import *
+from app.session.domain.models import *
+from app.user.domain.models import *
