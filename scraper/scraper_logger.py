ff94e25f2ba1064028393416fbdfe28b9e5434c8
bendeez <blaziken@MSI>
2024-09-19 15:49:34 -0500

    configured logger so there is  somewhere to look at the exceptions

--- scraper/scraper_logger.py
+++ scraper/scraper_logger.py
@@ -1,10 +1,16 @@
 import logging
 
-logging.basicConfig(
-    filename="scraper.log",
-    encoding="utf-8",
-    filemode="a",
-    format="{asctime} - {levelname} - {message}",
+logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler()
+file_handler = logging.FileHandler("scraper.log", mode="a", encoding="utf-8")
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+formatter = logging.Formatter(
+    "{asctime} - {levelname} - {message}",
     style="{",
     datefmt="%Y-%m-%d %H:%M",
 )
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+logger.setLevel("ERROR")
