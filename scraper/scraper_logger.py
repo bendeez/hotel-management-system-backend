74e149cc82708e94c2a58e4a6480fc15c6deb1e4
bendeez <blaziken@MSI>
2024-09-19 11:40:01 -0500

    added a logger

---
+++ scraper/scraper_logger.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(
+    filename="scraper.log",
+    encoding="utf-8",
+    filemode="a",
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+)
