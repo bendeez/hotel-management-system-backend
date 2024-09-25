ef746ddc67b2ec2189be59649d49cbd5f7172abe
bendeez <blaziken@MSI>
2024-09-25 11:45:26 -0500

    made some changes

--- admin/app/app.py
+++ admin/app/app.py
@@ -15,7 +15,7 @@ from app.tools.application.rate_limiter import limiter
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://chat.brisbanegateway.com.au"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
